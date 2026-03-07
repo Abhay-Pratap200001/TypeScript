b24237d8fe038ac4222f2dc05e898ace2d641eae
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2026-03-07 20:36:52 +0530

    Learn array in typescript

---
+++ src/ArrayTs.ts
@@ -0,0 +1,26 @@
+const chaiFlavours:string[] = ["Adrak Elachi", "Masala"]
+const chaiPrice:number[] = [20, 7]
+
+const rating: Array<number> = [4.5, 5.0]
+
+type Chai = {
+    name: string,
+    price: number
+}
+
+const menu: Chai[] = [
+    {name: "Adrak Elachi", price: 20},
+    {name: "Adrak", price: 25}
+]
+
+
+ const cities: readonly string[] = ["Lucknow", "Indore", "Mumbai"]
+
+
+const table: number[][] = [
+    [1,2,3],[4,5,6],[7,8,9]
+]
+
+let  chaiTuple: [string, number];
+chaiTuple = ['20', 20]
+ 
